48f4a146e8df740e22d548bef1a1d6309c9c35e7
jejeleterrible <jejeleterrible58@gmail.com>
2016-11-12 22:29:52 +0100

    remove blank lines between "GetGlobal" and the condition to test the type of the variable

--- Init.hpp
+++ Init.hpp
@@ -42,77 +42,62 @@ inline void InitFromLua(Nz::LuaInstance &lua)
 	if (lua.ExecuteFromFile("init.lua"))
 	{
 		lua.GetGlobal("width");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			width = (int)lua.ToInteger(-1);
 
 		lua.GetGlobal("height");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			height = (int)lua.ToInteger(-1);
 
 		lua.GetGlobal("title");
-
 		if (lua.GetType(-1) == Nz::LuaType_String)
 			title = lua.ToString(-1);
 
 		lua.GetGlobal("fullscreen");
-
 		if (lua.GetType(-1) == Nz::LuaType_Boolean)
 			fullscreen = lua.ToBoolean(-1);
 
 		lua.GetGlobal("sensitivity");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			sensitivity = (float)lua.ToNumber(-1);
 
 		lua.GetGlobal("speed");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			cameraSpeed = (float)lua.ToNumber(-1);
-
+		
 		lua.GetGlobal("skybox");
-
 		if (lua.GetType(-1) == Nz::LuaType_String)
 			file_skybox = lua.ToString(-1);
 
 		lua.GetGlobal("ground_width");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			ground_width = (float)lua.ToNumber(-1);
 
 		lua.GetGlobal("ground_height");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			ground_height = (float)lua.ToNumber(-1);
 
 		lua.GetGlobal("zFar");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			zFar = (float)lua.ToNumber(-1);
 
 		lua.GetGlobal("zNear");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			zNear = (float)lua.ToNumber(-1);
 
 		lua.GetGlobal("ground_texture");
-
 		if (lua.GetType(-1) == Nz::LuaType_String)
 			file_ground_tex = lua.ToString(-1);
 
 		lua.GetGlobal("light_type");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			light_type = (int)lua.ToInteger(-1);
 
 		lua.GetGlobal("gravity");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			gravity = (float)lua.ToNumber(-1);
 
 		lua.GetGlobal("eye_height");
-
 		if (lua.GetType(-1) == Nz::LuaType_Number)
 			eye_height = (float)lua.ToNumber(-1);
 
